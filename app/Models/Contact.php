b24b604fa1c1699815dfda4deb88961e8ebb8e2d
abdulah bakhach <abdullah.bakhach@gmail.com>
2022-12-21 16:38:52 +0200

    add AccountScope

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\scopes\AccountScope;
 
 class Contact extends Model
 {
     use HasFactory;
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope(new AccountScope);
+    }
 }
